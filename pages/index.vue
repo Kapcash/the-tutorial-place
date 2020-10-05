0459d7353ebbda09ce230a0ee6740be62177c5b4
florent.catiau <florent.catiau@veepee.com>
2020-10-05 22:54:02 +0200

    feat: fetch first content

--- pages/index.vue
+++ pages/index.vue
@@ -5,32 +5,25 @@
       <h1 class="title">
         the-tutorial-place
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <nuxt-content :document="document" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Context } from '@nuxt/types'
+import { Vue, Component } from 'vue-property-decorator'
 
-export default Vue.extend({})
+@Component
+export default class HomePage extends Vue {
+  document: any;
+
+  async asyncData ({ $content }: Context) {
+    const document = await $content('hello').fetch()
+
+    return { document }
+  }
+}
 </script>
 
 <style>

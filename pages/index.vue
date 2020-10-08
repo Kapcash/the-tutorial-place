1985e27f88f7809037430d581a3501a110b5bf95
florent.catiau <florent.catiau@veepee.com>
2020-10-08 19:46:12 +0200

    chore: update eslint rules

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
-    <Logo />
-    <nuxt-content :document="hello" />
+    <Logo/>
+    <nuxt-content :document="hello"/>
     <div class="flex flex-wrap md:justify-start justify-around">
-      <ArticleCard v-for="article of articles" :key="article.title" :article="article" />
+      <ArticleCard v-for="article of articles" :key="article.title" :article="article"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Context } from '@nuxt/types'
-import { Vue, Component } from 'vue-property-decorator'
+import { Context } from '@nuxt/types';
+import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class HomePage extends Vue {
   hello: any;
   articles: any[] = [];
 
-  async asyncData ({ $content }: Context) {
-    const hello = await $content('hello').fetch()
-    const articles = await $content('articles', { deep: true }).sortBy('createdAt').without(['body']).limit(10).fetch()
+  async asyncData({ $content }: Context) {
+    const hello = await $content('hello').fetch();
+    const articles = await $content('articles', { deep: true }).sortBy('createdAt').without(['body']).limit(10).fetch();
 
-    return { hello, articles }
+    return { hello, articles };
   }
 }
 </script>

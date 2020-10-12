503429bb44fd57d494736b0e214cb0cad3b4ede0
florent.catiau <florent.catiau@veepee.com>
2020-10-12 19:39:44 +0200

    feat: setup vuex + grid

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,10 +1,15 @@
 <template>
-  <input
-    :value="query"
-    @input="$emit('input', $event.target.value)"
-    type="search"
-    autocomplete="off"
-    :placeholder="placeholder">
+  <div class="relative w-full">
+    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+      <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-500"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
+    </div>
+    <input
+      v-model="query"
+      type="search"
+      autocomplete="off"
+      class="my-auto h-8 block w-full pl-10 pr-3 py-2 truncate leading-5 placeholder-gray-500 border border-transparent text-gray-700 dark:text-white dark-focus:text-white focus:border-gray-300 dark-focus:border-gray-700 rounded-md focus:outline-none focus:bg-white dark-focus:bg-gray-900 bg-gray-200 dark:bg-gray-800"
+      :placeholder="placeholder">
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,11 +18,16 @@ import { IContentDocument } from '@nuxt/content/types/content';
 
 @Component
 export default class SearchBar extends Vue {
-  @Prop({ type: String, required: true })
-  readonly query!: string;
-
   @Prop({ type: String, required: false, default: '' })
   readonly placeholder!: string;
+
+  get query() {
+    return this.$accessor.searchQuery;
+  }
+
+  set query(value) {
+    this.$accessor.searchFor(value);
+  }
 }
 </script>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,17 @@
   <div class="pt-12">
     <Navbar class="h-12"/>
     <main class="container mx-auto px-4 lg:px-8">
-      <div class="flex relative">
-        <Sidebar class="w-full lg:w-1/5 hidden"/>
-        <Nuxt class="flex flex-wrap w-full lg:w-4/5"/>
+      <div class="relative main-grid">
+        <Sidebar class="hidden"/>
+        <Nuxt class="flex flex-wrap w-full"/>
       </div>
     </main>
   </div>
 </template>
 
 <style scoped>
+.main-grid {
+  @apply grid gap-4;
+  grid-template-columns: 1fr 3fr;
+}
 </style>
